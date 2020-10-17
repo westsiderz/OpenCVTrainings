4b7018bf41204ae0338d82ba64d8dd842497de5b
Ahmed Karaibrahimov <frozen_heart88@abv.bg>
2020-10-17 03:46:16 -0400

    Update the CMake to use Linux specific OpenCV components

--- Cmake/OpenCVCMake.cmake
+++ Cmake/OpenCVCMake.cmake
@@ -1,9 +1,27 @@
 # Setting include and library directories and libraries 
 # related to OpenCV
 
-set(OPENCV_INCLUDE_DIRS /usr/local/include/opencv4)
+if(${CMAKE_SYSTEM_NAME} STREQUAL  "Linux")
+set(OPENCV_INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/../../opencv/build
+			 ${CMAKE_CURRENT_SOURCE_DIR}/../../opencv/include
+			 ${CMAKE_CURRENT_SOURCE_DIR}/../../opencv/modules/calib3d/include
+			 ${CMAKE_CURRENT_SOURCE_DIR}/../../opencv/modules/core/include
+			 ${CMAKE_CURRENT_SOURCE_DIR}/../../opencv/modules/dnn/include
+			 ${CMAKE_CURRENT_SOURCE_DIR}/../../opencv/modules/features2d/include
+			 ${CMAKE_CURRENT_SOURCE_DIR}/../../opencv/modules/flann/include
+			 ${CMAKE_CURRENT_SOURCE_DIR}/../../opencv/modules/gapi/include
+			 ${CMAKE_CURRENT_SOURCE_DIR}/../../opencv/modules/highgui/include
+			 ${CMAKE_CURRENT_SOURCE_DIR}/../../opencv/modules/imgcodecs/include
+			 ${CMAKE_CURRENT_SOURCE_DIR}/../../opencv/modules/imgproc/include
+			 ${CMAKE_CURRENT_SOURCE_DIR}/../../opencv/modules/ml/include
+			 ${CMAKE_CURRENT_SOURCE_DIR}/../../opencv/modules/objdetect/include
+			 ${CMAKE_CURRENT_SOURCE_DIR}/../../opencv/modules/photo/include
+			 ${CMAKE_CURRENT_SOURCE_DIR}/../../opencv/modules/stitching/include
+			 ${CMAKE_CURRENT_SOURCE_DIR}/../../opencv/modules/ts/include
+			 ${CMAKE_CURRENT_SOURCE_DIR}/../../opencv/modules/video/include
+			 ${CMAKE_CURRENT_SOURCE_DIR}/../../opencv/modules/videoio/include)
 
-set(OPENCV_LIBRARY_DIRS /usr/local/lib)
+set(OPENCV_LIBRARY_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/../../opencv/build/lib)
 
 set(OPENCV_LIBRARIES 	${OPENCV_LIBRARY_DIRS}/libopencv_calib3d.so
 			${OPENCV_LIBRARY_DIRS}/libopencv_core.so
@@ -20,6 +38,12 @@ set(OPENCV_LIBRARIES 	${OPENCV_LIBRARY_DIRS}/libopencv_calib3d.so
 			${OPENCV_LIBRARY_DIRS}/libopencv_stitching.so
 			${OPENCV_LIBRARY_DIRS}/libopencv_video.so
 			${OPENCV_LIBRARY_DIRS}/libopencv_videoio.so)
+			
+elseif(${CMAKE_SYSTEM_NAME} STREQUAL  "Windows")
+
+message("Windows build not implemented yet")
+			
+endif()
 
 target_include_directories(${PROJECT_NAME} PUBLIC ${OPENCV_INCLUDE_DIRS} )
 
