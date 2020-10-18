f9ae4a4254b0825eb62b2de32c463a87b3ee4ebf
Ahmed Karaibrahimov <frozen_heart88@abv.bg>
2020-10-18 19:36:49 +0300

    Update the example and Cmake file

--- HelloOpenCV/src/HelloOpenCV.cpp
+++ HelloOpenCV/src/HelloOpenCV.cpp
@@ -1,17 +1,22 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+
 using namespace cv;
+
 int main(int argc, char** argv )
 {
     Mat image;
-    image = imread( "../tesla.jpg", 1 );
+    image = imread( "../tesla.jpg", IMREAD_COLOR );
+
     if ( !image.data )
     {
         std::cout << "No image data \n";
         return -1;
     }
+
     namedWindow("Display Image", WINDOW_AUTOSIZE );
     imshow("Display Image", image);
+
     waitKey(0);
     return 0;
 }
