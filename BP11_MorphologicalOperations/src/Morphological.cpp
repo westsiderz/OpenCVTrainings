913a8641988c7a8024d7c0a900888b3cc8548508
Ahmed Karaibrahimov <frozen_heart88@abv.bg>
2021-01-07 22:20:16 +0200

    Add Morphological operations example

---
+++ BP11_MorphologicalOperations/src/Morphological.cpp
@@ -0,0 +1,54 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <string>
+
+using namespace cv;
+
+// Applies the Opening morphological operation 
+void applyOpening()
+{
+    // Path to the input image
+    std::string l_pathToInputImage{ "../Resources/loveSymbolNoisy.jpg" };
+
+    // Create an object to hold the image data of the first image
+    Mat l_image;
+    // Read the image date from a file with no change to color scheme
+    l_image = imread(l_pathToInputImage, IMREAD_UNCHANGED);
+
+	// Check if we have read the first image data correctly
+    if (!l_image.data)
+    {
+        std::cout << "No image data \n";
+        return;
+    }
+    
+    // Create the output image matrix
+    Mat l_outputImage = l_image.clone();
+    
+    // Creating the structuring element for the morphological operation
+    int l_openingSize = 9;
+    Mat l_openingElement = getStructuringElement( MORPH_RECT,
+                       Size( 2*l_openingSize + 1, 2*l_openingSize+1 ),
+                       Point( l_openingSize, l_openingSize ) );
+      
+    // Applying the Opening Morphological operation               
+    morphologyEx( l_image, l_outputImage, MORPH_OPEN, l_openingElement );
+		 
+
+    // Display the input image
+    namedWindow("Input", WINDOW_NORMAL);
+    cv::imshow("Input", l_image);
+
+    // Display the result image
+    namedWindow("Result", WINDOW_NORMAL);
+    cv::imshow("Result", l_outputImage);
+}
+
+
+int main()
+{
+    applyOpening();
+
+    waitKey(0);
+    return 0;
+}
