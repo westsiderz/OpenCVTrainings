f641efb60c5c6704176fb0cc5fee045668cf646c
kaa1sf <Ahmed.Karaibrahimov@bg.bosch.com>
2021-01-22 16:19:27 +0200

    Update the Hit-or-Miss example

--- BP12_HitAndMiss/src/HitAndMiss.cpp
+++ BP12_HitAndMiss/src/HitAndMiss.cpp
@@ -5,8 +5,8 @@
 using namespace cv;
 
 
-// Applies the morphological Gradient operation 
-void applyHitAndMiss()
+// Applies the Hit-or-Miss on a image
+void applyHitOrMissImage()
 {
     // Path to the input image
     std::string l_pathToInputImage{ "../Resources/loveSymbol.jpg" };
@@ -45,11 +45,46 @@ void applyHitAndMiss()
     cv::imshow("Result", l_outputImage);
 }
 
+// Applies the Hit-or-Miss on a set of data
+void applyHitOrMissMatrix()
+{
+    Mat l_image = (Mat_<uchar>(8, 10) <<
+        0, 0, 255, 0, 0, 0, 255, 0, 0, 0,
+        0, 0, 255, 0, 0, 0, 255, 0, 0, 0,
+        255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+        0, 0, 255, 0, 0, 0, 255, 0, 0, 0,
+        0, 0, 255, 0, 0, 0, 255, 0, 0, 0,
+        0, 0, 255, 0, 0, 0, 255, 0, 0, 0,
+        255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+        255, 255, 255, 255, 255, 255, 255, 255, 255, 255);
+
+    // Create the output image matrix
+    Mat l_outputImage = l_image.clone();
+
+    // Creating the structuring element for the morphological operation              
+    Mat kernel = (Mat_<int>(3, 3) <<
+        0,  0, 0,
+        0, -1, 0,
+        1,  1, 1);
+
+    // Applying the Gradient Morphological operation               
+    morphologyEx(l_image, l_outputImage, MORPH_HITMISS, kernel);
+
+
+    // Display the input image
+    namedWindow("Input", WINDOW_NORMAL);
+    cv::imshow("Input", l_image);
+
+    // Display the result image
+    namedWindow("Result", WINDOW_NORMAL);
+    cv::imshow("Result", l_outputImage);
+}
 
 
 int main()
 {
-    applyHitAndMiss();
+    //applyHitOrMissImage();
+    applyHitOrMissMatrix();
 
     waitKey(0);
     return 0;
