0460fb3d046f219919ef85699aa4711fe8267ea6
Ahmed Karaibrahimov <frozen_heart88@abv.bg>
2021-02-08 07:18:55 +0200

    Add InRange example

--- BP16_Thresholding/src/Thresholding.cpp
+++ BP16_Thresholding/src/Thresholding.cpp
@@ -45,9 +45,43 @@ void applyThreshold()
     cv::imshow("Result", l_outputImage);
 }
 
+// Apply threshold to a color image
+void applyInRangeThreshold()
+{
+    // Path to the input image
+    std::string l_pathToInputImage{ "../Resources/car.jpg" };
+
+    // Create an object to hold the image data of the first image
+    Mat l_image;
+    // Read the image date from a file with no change to color scheme
+    l_image = imread(l_pathToInputImage, IMREAD_UNCHANGED);
+
+    // Check if we have read the first image data correctly
+    if (!l_image.data)
+    {
+        std::cout << "No image data \n";
+        return;
+    }
+
+    // Create the output image matrices
+    Mat l_outputImage;
+
+    // Apply thresholding
+    inRange(l_image, Scalar(0, 0, 0), Scalar(50,50,50), l_outputImage);
+
+    // Display the input image
+    namedWindow("Input", WINDOW_NORMAL);
+    cv::imshow("Input", l_image);
+
+    // Display the result image
+    namedWindow("Result", WINDOW_NORMAL);
+    cv::imshow("Result", l_outputImage);
+}
+
 int main()
 {
-    applyThreshold();
+    //applyThreshold();
+    applyInRangeThreshold();
 
     waitKey(0);
     return 0;
