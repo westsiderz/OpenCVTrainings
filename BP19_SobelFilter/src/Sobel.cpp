46327c66b4d5ba682df43d186585680f208da4b2
Ahmed Karaibrahimov <frozen_heart88@abv.bg>
2021-02-28 15:56:44 +0200

    Add spatial gradient example

--- BP19_SobelFilter/src/Sobel.cpp
+++ BP19_SobelFilter/src/Sobel.cpp
@@ -57,6 +57,55 @@ void applySobelFilter()
     cv::imshow("Result", l_outputImage);
 }
 
+// Apply the Sobel filter to an image using spatial gradient
+void applySpatialGradient()
+{
+    // Path to the input image
+    std::string l_pathToInputImage{ "../Resources/youngBlood.jpg" };
+
+    // Create an object to hold the image data of the first image
+    Mat l_image;
+    // Read the image date from a file with no change to color scheme
+    l_image = imread(l_pathToInputImage, IMREAD_GRAYSCALE);
+
+    // Check if we have read the first image data correctly
+    if (!l_image.data)
+    {
+        std::cout << "No image data \n";
+        return;
+    }
+
+    // Create the output image matrices
+    Mat l_outputImage{};
+
+    // Create the Sobel results
+    Mat l_xSobel{};
+    Mat l_ySobel{};
+    constexpr int c_size = 3;
+
+    // Apply the spatial gradient
+    spatialGradient(l_image, l_xSobel, l_ySobel, c_size);
+
+    // Convert back to CV_8U
+    Mat l_xAbsSobel{};
+    Mat l_yAbsSobel{};
+    convertScaleAbs(l_xSobel, l_xAbsSobel);
+    convertScaleAbs(l_ySobel, l_yAbsSobel);
+
+    // Combine both X and Y direction results
+    constexpr double c_alpha = 0.5;
+    constexpr double c_beta = 0.5;
+    addWeighted(l_xAbsSobel, c_alpha, l_yAbsSobel, c_beta, 0, l_outputImage);
+
+    // Display the input image
+    namedWindow("Input", WINDOW_NORMAL);
+    cv::imshow("Input", l_image);
+
+    // Display the result image
+    namedWindow("Result", WINDOW_NORMAL);
+    cv::imshow("Result", l_outputImage);
+}
+
 // Apply the Scharr filter to an image
 void applyScharrFilter()
 {
@@ -112,8 +161,9 @@ void applyScharrFilter()
 
 int main()
 {
-    //applySobelFilter();
-    applyScharrFilter();
+    applySobelFilter();
+    //applySpatialGradient();
+    //applyScharrFilter();
 
     waitKey(0);
     return 0;
