61352ee3f49884b911ba193124ed9a0c7246d11b
Ahmed Karaibrahimov <frozen_heart88@abv.bg>
2021-03-07 18:16:33 +0200

    Update the transformations examples

--- BP21_Transformations/src/Transformations.cpp
+++ BP21_Transformations/src/Transformations.cpp
@@ -315,6 +315,58 @@ void applyRotation()
     cv::imshow("Result Clockwise", l_outputImage2);
 }
 
+// Apply inverse transformation to an image
+void applyInverseTransformation()
+{
+    // Path to the input image
+    std::string l_pathToInputImage{ "../Resources/stormrider.jpg" };
+
+    // Create an object to hold the image data of the first image
+    Mat l_image;
+    // Read the image date from a file with no change to color scheme
+    l_image = imread(l_pathToInputImage, IMREAD_UNCHANGED);
+
+    // Check if we have read the first image data correctly
+    if (!l_image.data)
+    {
+        std::cout << "No image data \n";
+        return;
+    }
+
+    // Create the output image matrices
+    Mat l_outputImage1;
+    Mat l_outputImage2;
+
+    // Set rotation properties
+    Point2f l_rotationCenter{ l_image.cols / 2.0f, l_image.rows / 2.0f };
+    constexpr double c_rotationAngleCClockwise = 45.0;
+    constexpr double c_scale = 1.0;
+
+    // Calculate the rotation transformation matrices
+    Mat l_rotationCounterClockwiseMatrix = getRotationMatrix2D(
+        l_rotationCenter,
+        c_rotationAngleCClockwise,
+        c_scale);
+
+    // Invert the transformation matrix
+    Mat l_inverseTransformation{};
+    invertAffineTransform(l_rotationCounterClockwiseMatrix, l_inverseTransformation);
+
+    // Apply the transformations
+    warpAffine(l_image, l_outputImage1, l_rotationCounterClockwiseMatrix, l_outputImage1.size());
+    warpAffine(l_image, l_outputImage2, l_inverseTransformation, l_outputImage2.size());
+
+    // Display the input image
+    namedWindow("Input", WINDOW_NORMAL);
+    cv::imshow("Input", l_image);
+
+    // Display the result images
+    namedWindow("Result Counter Clockwise", WINDOW_NORMAL);
+    cv::imshow("Result Counter Clockwise", l_outputImage1);
+
+    namedWindow("Result Inverse", WINDOW_NORMAL);
+    cv::imshow("Result Inverse", l_outputImage2);
+}
 
 int main()
 {
@@ -322,7 +374,8 @@ int main()
     //applyReflection();
     //applyScaling();
     //applyResizing();
-    applyRotation();
+    //applyRotation();
+    applyInverseTransformation();
 
     waitKey(0);
     return 0;
