13292355153b096e61f1d52bee51f28f098a9327
kaa1sf <Ahmed.Karaibrahimov@bg.bosch.com>
2021-08-26 08:45:52 +0300

    Updated the Transformation example

--- BP29_Transformations/src/Transformations.cpp
+++ BP29_Transformations/src/Transformations.cpp
@@ -1,6 +1,7 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <string>
+#include <math.h>
 
 using namespace cv;
 
@@ -368,6 +369,124 @@ void applyInverseTransformation()
     cv::imshow("Result Inverse", l_outputImage2);
 }
 
+// Apply perspective transformation to an image
+void applyPerspectiveTransformation()
+{
+    // Path to the input image
+    std::string l_pathToInputImage{ "../Resources/city.jpg" };
+
+    // Create an object to hold the image data of the first image
+    Mat l_image;
+    // Read the image date from a file with no change to color scheme
+    l_image = imread(l_pathToInputImage, IMREAD_UNCHANGED);
+
+    // Check if we have read the first image data correctly
+    if (!l_image.data)
+    {
+        std::cout << "No image data \n";
+        return;
+    }
+
+    // Create the output image matrices
+    Mat l_outputImage1;
+    Mat l_outputImage2;
+
+    // Set the initial and end points of the first transformation
+    Point2f l_initialPoint[]
+    {
+        {0.0f, 0.0f},                                  // Point 1 coordinates
+        {l_image.cols - 1.0f, 0.0f},                   // Point 2 coordinates
+        {0.0f, l_image.rows - 1.0f},                   // Point 3 coordinates
+        {l_image.cols - 1.0f, l_image.rows - 1.0f}     // Point 4 coordinates
+    };
+
+    Point2f l_endPoint[]
+    {
+        {150.0f, 150.0f},                             // Point 1 coordinates
+        {l_image.cols - 1.0f - 150.0f, 150.0f},       // Point 2 coordinates
+        {0.0f, l_image.rows - 1.0f},                  // Point 3 coordinates
+        {l_image.cols - 1.0f, l_image.rows - 1.0f}    // Point 4 coordinates
+    };
+
+    // Set the initial and end points as arrays of the second transformation
+    Mat l_srcArray = (Mat_<float>(4, 2) <<
+        0.0f, 0.0f,                                    // Point 1 coordinates
+        l_image.cols - 1.0f, 0.0f,                     // Point 2 coordinates
+        0.0f, l_image.rows - 1.0f,                     // Point 3 coordinates
+        l_image.cols - 1.0f, l_image.rows - 1.0f       // Point 4 coordinates
+        );
+
+    Mat l_dstArray = (Mat_<float>(4, 2) <<
+        0.0f, 0.0f,                                                   // Point 1 coordinates
+        l_image.cols - 1.0f, 0.0f,                                    // Point 2 coordinates
+        150.0f, l_image.rows - 1.0f - 150.0f,                         // Point 3 coordinates
+        l_image.cols - 1.0f - 150.0f, l_image.rows - 1.0f - 150.0f    // Point 4 coordinates
+        );
+
+    // Calculate the transformation matrices
+    Mat l_transformationMatrix1 = getPerspectiveTransform(l_initialPoint, l_endPoint);
+    Mat l_transformationMatrix2 = getPerspectiveTransform(l_srcArray, l_dstArray);
+
+    // Apply the transformations
+    warpPerspective(l_image, l_outputImage1, l_transformationMatrix1, l_outputImage1.size());
+    warpPerspective(l_image, l_outputImage2, l_transformationMatrix2, l_outputImage2.size());
+
+
+    // Display the input image
+    namedWindow("Input", WINDOW_NORMAL);
+    cv::imshow("Input", l_image);
+
+    // Display the result images
+    namedWindow("Result 1", WINDOW_NORMAL);
+    cv::imshow("Result 1", l_outputImage1);
+
+    namedWindow("Result 2", WINDOW_NORMAL);
+    cv::imshow("Result 2", l_outputImage2);
+}
+
+// Apply remapping to an image
+void applyRemapTransformation()
+{
+    // Path to the input image
+    std::string l_pathToInputImage{ "../Resources/stormrider.jpg" };
+
+    // Create an object to hold the image data of the first image
+    Mat l_image;
+    // Read the image date from a file with no change to color scheme
+    l_image = imread(l_pathToInputImage, IMREAD_UNCHANGED);
+
+    // Check if we have read the first image data correctly
+    if (!l_image.data)
+    {
+        std::cout << "No image data \n";
+        return;
+    }
+
+    // Create the output image and remap matrices
+    Mat l_outputImage{  l_image.size(), l_image.type() };
+    Mat l_remapX{ l_image.size(), CV_32FC1 };
+    Mat l_remapY{ l_image.size(), CV_32FC1 };
+    
+    // Prepare remap matrices
+    for (size_t i = 0; i < l_image.rows; ++i)
+    {
+        for (size_t j = 0; j < l_image.cols; ++j)
+        {
+            l_remapX.at<float>(i, j) = static_cast<float>(l_image.cols - (j * 0.5f));
+            l_remapY.at<float>(i, j) = static_cast<float>(i);
+        }
+    }
+
+    remap(l_image, l_outputImage, l_remapX, l_remapY, INTER_LINEAR, BORDER_CONSTANT, Scalar(0, 0, 0));
+
+    // Display the input image
+    namedWindow("Input", WINDOW_NORMAL);
+    cv::imshow("Input", l_image);
+
+    namedWindow("Result Inverse", WINDOW_NORMAL);
+    cv::imshow("Result Inverse", l_outputImage);
+}
+
 int main()
 {
     //applyTranslation();
@@ -375,7 +494,9 @@ int main()
     //applyScaling();
     //applyResizing();
     //applyRotation();
-    applyInverseTransformation();
+    //applyInverseTransformation();
+    //applyPerspectiveTransformation();
+    applyRemapTransformation();
 
     waitKey(0);
     return 0;
